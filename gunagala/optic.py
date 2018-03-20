740162ec13df36c1d905c345bc7e1d0d58ce38bd
Anthony Horton <anthony.horton@aao.gov.au>
2018-03-20 18:36:57 +1100

    Add function to create throughput models from mirror coating data

--- gunagala/optic.py
+++ gunagala/optic.py
@@ -3,12 +3,13 @@ Optics, e.g. a telescope or lens
 """
 
 import os
+import functools
 import numpy as np
 
 from astropy import units as u
 from astropy.table import Table
 
-from gunagala.utils import ensure_unit, get_table_data
+from gunagala.utils import ensure_unit, get_table_data, array_sequence_equal
 
 
 class Optic:
@@ -89,6 +90,78 @@ class Optic:
 
         self.theta_range = u.Quantity((theta_min, theta_max)).to(u.degree)
 
-        self.wavelengths, self.throughput = get_table_data(throughput, data_dir='data/performance_data',
-                                                           column_names = ('Wavelength', 'Throughput'),
-                                                           column_units = (u.nm, u.dimensionless_unscaled))
+        self.wavelengths, self.throughput = get_table_data(throughput,
+                                                           column_names=('Wavelength', 'Throughput'),
+                                                           column_units=(u.nm, u.dimensionless_unscaled))
+
+def make_throughput(surfaces):
+    """
+    Constructs a table of throughout vs wavelength from the numbers and types of optical surfaces
+
+    Utility function to provide a simple estimate of optical throughput versus wavelength given
+    the numbers and types of optical surfaces. Uses tabulated data included within gunagala for
+    a number of common optical surfaces. To get a list of the available surface types call
+    list_surfaces(). Mirror coating data from by Thorlabs (http://www.tholabs.com/)
+
+    Parameters
+    ----------
+    surfaces: list of tuples
+        List containing tuples of (surface name, )
+
+    Returns
+    -------
+    table: astropy.table.Table
+        Table with columns Wavelength and Throughput
+    """
+    wavelengths_list = []
+    throughputs_list = []
+    for surface_name, surface_number in surfaces:
+        assert surface_name in list_surfaces(), \
+            "{} is not one the supported surface types!".format(surface_name)
+        if _surfaces[surface_name][:8] == 'Thorlabs':
+            # Thorlabs mirror coating data. Needs some extra keyword arguments.
+            wavelengths, throughputs = get_table_data(_surfaces[surface_name],
+                                                      column_names=('Wavelength', 'Reflectance'),
+                                                      column_units=(u.um, u.percent),
+                                                      names=('Comments 1', 'Comments 2',
+                                                             'Wavelength', 'Reflectance P',
+                                                             'Reflectance S', 'Reflectance'),
+                                                      encoding='utf-8')
+        else:
+            wavelengths, throughputs = get_table_data(_surfaces[surface_name],
+                                                      column_names=('Wavelength', 'Transmission'),
+                                                      column_units=(u.nm, u.dimensionless_unscaled))
+        wavelengths_list.append(wavelengths)
+        throughputs = throughputs.to(u.dimensionless_unscaled)
+        throughputs = throughputs**surface_number
+        throughputs_list.append(throughputs)
+
+    if len(wavelengths_list) == 1:
+        # Only one surface type.
+        throughput_table = Table((wavelengths_list[0], throughputs_list[0]),
+                                 names=('Wavelength', 'Throughput'))
+    elif array_sequence_equal(wavelengths_list):
+        # All surfaces have the same wavelength sampling, easy to combine.
+        throughput_combined = functools.reduce(lambda x, y: x*y, throughputs_list)
+        throughput_table = Table((wavelengths_list[0], throughput_combined),
+                                 names=('Wavelength', 'Throughput'))
+    else:
+        # Some surfaces use different wavelength sampling. Need to resample before combining.
+        raise NotImplementedError("Surface types have different wavelength sampling.")
+
+    return throughput_table
+
+
+def list_surfaces():
+    surfaces = list(_surfaces.keys())
+    surfaces.sort()
+    return surfaces
+
+
+_surfaces = {'aluminium_12deg_protected': 'Thorlabs_Protected_Aluminum_Coating_12deg.csv',
+             'aluminium_45deg_protected': 'Thorlabs_Protected_Aluminum_Coating_45deg.csv',
+             'gold_12deg_protected': 'Thorlabs_Protected_Gold_Coating_12deg.csv',
+             'gold_45deg_protected': 'Thorlabs_Protected_Gold_Coating_45deg.csv',
+             'silver_12deg_protected': 'Thorlabs_Protected_Silver_Coating_12deg.csv',
+             'silver_45deg_protected': 'Thorlabs_Protected_Silver_Coating_45deg.csv',
+             'gold_45deg_unprotected': 'Thorlabs_Unprotected_Gold_Coating_45deg.csv'}
--- gunagala/utils/__init__.py
+++ gunagala/utils/__init__.py
@@ -3,6 +3,7 @@
 # This sub-module is destined for common non-package specific utility
 # functions that will ultimately be merged into `astropy.utils`
 import os
+import functools
 import astropy.units as u
 from astropy.table import Table
 from astropy.utils.data import get_pkg_data_filename
@@ -40,7 +41,33 @@ def ensure_unit(arg, unit):
     return arg
 
 
-def get_table_data(data_table, data_dir, column_names, column_units):
+def get_table_data(data_table, column_names, column_units,
+                   data_dir='data/performance_data', **kwargs):
+    """
+    Parses a data table to extract specified columns, converted to Quantity with specified units.
+
+    Parameters
+    ----------
+    data_table: astropy.table.Table or str
+        The data table for parsing, either as an astropy.table.Table object or the name of a file
+        that can be read by `astropy.table.Table.read()`. The filename can be either the path to a
+        user file or the name of one of gunagala's included files.
+    column_names: sequence
+        Names of the columns to extract from the table
+    column_units: sequence
+        Desired units for the extracted columns. If data_table specifies units for its columns then
+        the extracted columns will be converted to these units. If not then the specified units
+        will be added to the corresponding column.
+
+    Additional keyword arguments will be passed to the call to astropy.table.Table.read()
+    if reading a Table from a file. See the documentation for Table.read() for details
+    of the available parameters.
+
+    Returns
+    -------
+    data: tuple of astropy.units.Quantity
+        Tuple of Quantity objects corresponding to the named columns, with the specified units.
+    """
     if not isinstance(data_table, Table):
         # data_table isn't a Table, assume it's a filename.
         if not os.path.exists(data_table):
@@ -51,7 +78,7 @@ def get_table_data(data_table, data_dir, column_names, column_units):
             except:
                 # Not in package data directories either
                 raise IOError("Couldn't find data table {}!".format(data_table))
-        data_table = Table.read(data_table)
+        data_table = Table.read(data_table, **kwargs)
 
     data = []
     for name, unit in zip(column_names, column_units):
@@ -66,3 +93,31 @@ def get_table_data(data_table, data_dir, column_names, column_units):
             data.append(column.quantity.to(unit))
 
     return data
+
+
+def array_sequence_equal(array_sequence, reference=None):
+    """
+    Determine if all array objects in a sequence are equal.
+
+    Parameters
+    ----------
+    array_sequence: sequence of numpy.array
+        Sequence of numpy.array or compatible type (e.g. astropy.unit.Quantity) objects to compare.
+        The objects must support element-wise comparision and implement an any() method.
+    reference: numpy.array, optional
+        If given all arrays in the sequence will be compared with reference, otherwise they will
+        be compared with each other.
+
+    Returns
+    -------
+    equal: bool
+        True if all arrays in the sequence are equal (or equal to reference, if given), otherwise
+        False.
+    """
+    if len(array_sequence) < 2:
+        return True
+    else:
+        if reference is None:
+            reference = array_sequence[0]
+        comparisons = map(lambda x: (x==reference).all(), array_sequence)
+        return functools.reduce(lambda x, y: x and y, comparisons)
